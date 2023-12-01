49ce9caf3e17c4c41ec5def2978bb4fc5b1a44c6
Dailenlink <dailensenica@hotmail.com>
2023-12-01 09:39:28 +0100

    Basic paths

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,13 @@ from app import app
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/newpath')
+def newpath():
+    return render_template('new-path.html')
+
+@app.route('/mypaths')
+def mypaths():
+    return render_template('my-paths.html')
+
+# @app.route('/path/<int:id>')
