ce7d505da3546d809530ff16c616a2362e6efddb
gert-jangillis <gertjangillis8@gmail.com>
2023-12-27 17:26:48 +0100

    flask routes and html pages

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,16 @@ from app import app
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# Define what you want to learn
+@app.route('/learningpath')
+def learningpath():
+    return render_template('learningpath.html')
+
+# Explanations page
+@app.route('/recommendation')
+def recommendation():
+    return render_template('recommendation.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
