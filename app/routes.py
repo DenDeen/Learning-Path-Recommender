d75cc601195c0df7ca34df22aa93965b723e25d1
gert-jangillis <gertjangillis8@gmail.com>
2024-01-06 18:08:13 +0100

    survey check

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, stream_with_context, Response
+from flask import render_template, request, stream_with_context, Response, send_file
 from app import app
 from app.utils import models as model_utils
 
@@ -94,3 +94,15 @@ def output():
     # Get processed data from the session variable
     # You can now display the output and integrate chat functionality
     return render_template("output.html")
+
+@app.route("/survey")
+def survey():
+    # Get processed data from the session variable
+    # You can now display the output and integrate chat functionality
+    return render_template("survey.html")
+
+@app.route('/download')
+def download_file():
+    # Path to the file you want to serve
+    file_path = 'static/resources/informatiebrief.pdf'
+    return send_file(file_path, as_attachment=True)
